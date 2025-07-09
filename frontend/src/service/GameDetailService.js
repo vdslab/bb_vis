5bdf1eca8f1fcda5a43d734918c7697af889f8d4
Kohsei Mochizuki <kose@KohseinoMacBook-Pro.local>
2025-07-10 08:50:34 +0900

    #3 feat: サービス層の追加

---
+++ frontend/src/service/GameDetailService.js
@@ -0,0 +1,15 @@
+import { ApiService } from "./ApiService";
+
+export class GameDetailService {
+  /**
+   * 試合IDから詳細を取得
+   * @param {string} gamepk - 試合ID
+   * @returns {Promise} - 試合詳細データ
+   */
+  static async getDetailById(gamepk) {
+    const endpoint = "api/gameDetail/getDetailById";
+    return ApiService.callGetApi(endpoint, {
+      gamepk: gamepk,
+    });
+  }
+}
