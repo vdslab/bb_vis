070191247101d78ebccb45409c62e7baeb25c42a
mituishi <chhr22067@g.nihon-u.ac.jp>
2025-07-25 15:15:29 +0900

    #32 feat: ロゴの色と不要なpngを削除

--- frontend/src/components/common/Header.jsx
+++ frontend/src/components/common/Header.jsx
@@ -1,10 +1,19 @@
-import rogoImg from "../../asset/rogo.png"; // 画像ファイルのインポート
+import rogoImg from "../../asset/rogo.png";
 
 const Header = () => {
   return (
     <header className="header">
       <h1>
-        <img src={rogoImg} alt="ロゴ" className="logo-img" />
+        <img
+          src={rogoImg}
+          alt="ロゴ"
+          style={{
+            width: "30%",
+            height: "60%",
+            position: "relative",
+            bottom: "-10px",
+          }}
+        />
       </h1>
       <nav>
         <span>ナビゲーション</span>
