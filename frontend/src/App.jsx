a347ab690764718e555ee6b367d8bc2bc88d62fd
Kohsei Mochizuki <kose@KohseinoMacBook-Pro.local>
2025-07-27 23:43:32 +0900

    #45 feat: サイドバーの実装とそれに伴うスタイルの修正

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,32 @@
 import "./App.css";
 import "./styles/global.css";
 import "./styles/layout.css";
+import { useState } from "react";
 import Header from "./components/common/Header";
 import TestHeader from "./components/common/DevHeader";
+import SideBar from "./components/common/SideBar";
 import Layout from "./components/layout/Layout";
 
 function App() {
+  const [sidebarExpanded, setSidebarExpanded] = useState(true);
+
+  const handleSidebarToggle = (isExpanded) => {
+    setSidebarExpanded(isExpanded);
+  };
+
   return (
     <div className="app-container">
+      {/* サイドバー */}
+      <SideBar onToggle={handleSidebarToggle} isOpen={sidebarExpanded} />
+
       {/* 本番用ヘッダー */}
       <Header />
       {/* 開発用ヘッダー */}
       {/*<TestHeader /> */}
 
-      <main className="main-container">
+      <main
+        className={`main-container ${sidebarExpanded ? "main-with-sidebar" : "main-without-sidebar"}`}
+      >
         <Layout />
       </main>
     </div>
