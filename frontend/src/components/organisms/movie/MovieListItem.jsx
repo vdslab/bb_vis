2692efc4b339d2e5d586ef06c1491c87e7f1f90d
Kohsei Mochizuki <kose@KohseinoMacBook-Pro.local>
2025-07-30 05:32:54 +0900

    #12 feat: ボタン・指によるスクロールとスタイルの最適化

--- frontend/src/components/organisms/movie/MovieListItem.jsx
+++ frontend/src/components/organisms/movie/MovieListItem.jsx
@@ -1,20 +1,23 @@
 import React, { memo } from "react";
 import "@styles/movie.css";
 
-const MovieListItem = memo(({ iframeTag, width, height }) => {
+const MovieListItem = memo(({ iframeTag }) => {
   // iframeTagの中でwidth/heightを動的に設定
   const modifiedIframeTag = iframeTag?.replace(
     /<iframe([^>]*?)>/i,
-    `<iframe$1 style="width: ${width}; height: ${height}; border: none;">`,
+    `<iframe$1 style="width: 100%; height: 100%; border: none; aspect-ratio: 16/9;">`,
   );
 
   return (
     <div className="movie-item">
-      <div
+      <div className="movie-item-iframe"
         dangerouslySetInnerHTML={{
           __html: modifiedIframeTag || "",
         }}
       />
+      <div className="movie-item-description">
+        description
+      </div>
     </div>
   );
 });
