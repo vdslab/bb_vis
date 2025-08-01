5f460ae5aa1c4120e97d89b8a5765405a80a54be
Kohsei Mochizuki <kose@KohseinoMacBook-Pro.local>
2025-08-01 12:26:22 +0900

    #67 fix: カードクリックで選択ゲーム情報が更新されるように

--- frontend/src/components/organisms/game/GameListCard.jsx
+++ frontend/src/components/organisms/game/GameListCard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-import { setGamePk, setHighlightData } from "@/store/GameStore";
+import { setGamePk, setHighlightData, setSelectedGameAwayTeam, setSelectedGameHomeTeam, setSelectedGameDate } from "@/store/GameStore";
 
 import {
   Card,
@@ -26,6 +26,9 @@ const GameListCard = ({
   const handleClick = () => {
     dispatch(setGamePk(gamepk));
     dispatch(setHighlightData(gamepk));
+    dispatch(setSelectedGameDate(date));
+    dispatch(setSelectedGameAwayTeam(awayteam));
+    dispatch(setSelectedGameHomeTeam(hometeam));
   };
 
   return (
