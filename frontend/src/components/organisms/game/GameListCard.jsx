8d5ff20593e90ec62c0e78bed9f954c5587ad73e
Kohsei Mochizuki <kose@KohseinoMacBook-Pro.local>
2025-10-29 20:10:43 +0900

    #98 feat: アコーディオンの開閉処理実装

--- frontend/src/components/organisms/game/GameListCard.jsx
+++ frontend/src/components/organisms/game/GameListCard.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { setGamePk, setHighlightData, setSelectedGameAwayTeam, setSelectedGameHomeTeam, setSelectedGameDate, setHighlightFromParallelCoordinates } from "@/store/GameStore";
 import "@/styles/gamelistcard.css";
+import GameListCardDetail from "./GameListCardDetail";
 
 const GameListCard = ({
   gamepk,
@@ -13,7 +14,7 @@ const GameListCard = ({
   isHighlighted = false,
 }) => {
   const dispatch = useDispatch();
-
+  const [ isOpen, setIsOpen ] = useState(false);
   const handleClick = () => {
     dispatch(setGamePk(gamepk));
     dispatch(setHighlightData(gamepk));
@@ -21,41 +22,46 @@ const GameListCard = ({
     dispatch(setSelectedGameDate(date));
     dispatch(setSelectedGameAwayTeam(awayteam));
     dispatch(setSelectedGameHomeTeam(hometeam));
+    setIsOpen(isOpen ? false : true)
   };
 
   return (
-    <div 
-      className={`game-list-card ${isHighlighted ? "highlighted" : ""}`}
-      onClick={handleClick}
-    >
-      {isHighlighted && (
-        <div className="game-list-card-pulse-indicator" />
-      )}
-      <div className="game-list-card-date">
-        {date.replace(/-/g, "/")}
-      </div>
-      <div className="game-list-card-teams">
-        {/* ホームチーム名 */}
-        <div className="game-list-card-team-name">
-          {hometeam}
+    <>
+      <div 
+        className={`game-list-card ${isHighlighted ? "highlighted" : ""}`}
+        onClick={handleClick}
+      >
+        {isHighlighted && (
+          <div className="game-list-card-pulse-indicator" />
+        )}
+        <div className="game-list-card-date">
+          {date.replace(/-/g, "/")}
         </div>
-        {/* ホームチームスコア */}
-        <span className="game-list-card-score score-end">
-          {hometeamscore}
-        </span>
-        <span className="game-list-card-score score-center">
-          -
-        </span>
-        {/* アウェーチームスコア */}
-        <span className="game-list-card-score score-start">
-          {awayteamscore}
-        </span>
-        {/* アウェーチーム名 */}
-        <div className="game-list-card-team-name">
-          {awayteam}
+        <div className="game-list-card-teams">
+          {/* ホームチーム名 */}
+          <div className="game-list-card-team-name">
+            {hometeam}
+          </div>
+          {/* ホームチームスコア */}
+          <span className="game-list-card-score score-end">
+            {hometeamscore}
+          </span>
+          <span className="game-list-card-score score-center">
+            -
+          </span>
+          {/* アウェーチームスコア */}
+          <span className="game-list-card-score score-start">
+            {awayteamscore}
+          </span>
+          {/* アウェーチーム名 */}
+          <div className="game-list-card-team-name">
+            {awayteam}
+          </div>
         </div>
       </div>
-    </div>
+      {isOpen && 
+      <GameListCardDetail />}
+    </>
   );
 };
 
---
+++ frontend/src/components/organisms/game/GameListCardDetail.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "@styles/gamelistcarddetail.css"
+
+const GameListCardDetail = () => {
+  // アコーディオンで試合の情報を表示する
+  // cssで大きさを決める
+  return (
+    <div className="game-list-card-detail">
+
+      <a>試合情報</a>
+      <a>何を表示するかは考えていない</a>
+    </div>
+  );
+}
+
+export default GameListCardDetail;
