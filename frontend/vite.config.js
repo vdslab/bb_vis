cf6a66829d60ec81116e55f4a45724002292c730
Kohsei Mochizuki <kose@KohseinoMacBook-Pro.local>
2025-11-20 20:36:53 +0900

    #109 feat:devonlyblockを使えるように

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,41 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import replace from "@rollup/plugin-replace";
 import path from "path";
 import { fileURLToPath } from "url";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@styles": path.resolve(__dirname, "./src/styles"),
+export default defineConfig(({mode}) => {
+  const isProd = mode === "production";
+  return {
+    plugins: [
+      {
+        ...replace({
+          delimiters: ["", ""],
+          preventAssignment: false,
+          values: isProd
+            ? {
+                "{/* devonly:start */}": "{/*",
+                "{/* devonly:end */}": "*/}",
+                "// devonly:start": "/*",
+                "// devonly:end": "*/",
+              }
+            : {},
+        }),
+        enforce: "pre",
+        apply() {
+          return isProd;
+        },
+      },
+      react()
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+        "@styles": path.resolve(__dirname, "./src/styles"),
+      },
     },
-  },
+  };
 });
