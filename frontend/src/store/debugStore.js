dbe9bb6e3fe379bf9112a21b4393983c3d2ec9a2
Kohsei Mochizuki <kose@KohseinoMacBook-Pro.local>
2025-11-22 18:17:55 +0900

    #115 feat:動画のオートスクロールを管理する状態を追加

--- frontend/src/store/debugStore.js
+++ frontend/src/store/debugStore.js
@@ -3,6 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 // debugモードに関するstoreを管理する
 const initialState = {
   debugMode:true,
+  movieAutoScroll:false,
 };
 
 const debugSlice = createSlice({
@@ -12,11 +13,15 @@ const debugSlice = createSlice({
     setDebugMode: (state, action) => {
       state.debugMode = action.payload;
     },
+    setMovieAutoScroll: (state, action) => {
+      state.movieAutoScroll = action.payload;
+    },
   },
 });
 
 export const {
   setDebugMode,
+  setMovieAutoScroll,
 } = debugSlice.actions;
 
 export default debugSlice.reducer;
