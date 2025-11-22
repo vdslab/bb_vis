e16d5f8866118dfe79157b7cb1c1d5ead81011d1
Kohsei Mochizuki <kose@KohseinoMacBook-Pro.local>
2025-11-23 00:41:12 +0900

    #115 fix:build通らない問題を解消

--- frontend/src/store/indexStore.js
+++ frontend/src/store/indexStore.js
@@ -1,12 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import gameReducer from "./GameStore";
+// devonly:start
 import debugReducer from "./DebugStore";
+// devonly:end
 
 // storeを統合
 const indexStore = configureStore({
   reducer: {
     game: gameReducer,
+    // devonly:start
     debug: debugReducer,
+    // devonly:end
   },
 });
 
